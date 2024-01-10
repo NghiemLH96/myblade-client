49edb264483554852a608d8e5d8ab3b83f9ec16f
Nghiem Le <nghiemle@Nghiems-MacBook-Pro.local>
2024-01-10 21:31:44 +0700

    client

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,17 +1,19 @@
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../../assets/blade-high-resolution-logo-black-removebg-preview.png'
 
 export default function Header() {
+  const navigate = useNavigate()
   return (
     <div className='pageHeader'>
       <div className="header-L">
         <img src={logo} className="logo" alt="" />
       </div>
       <div className="header-M">
-        <a href="">Home</a>
-        <a href="">Products</a>
-        <a href="">Account</a>
-        <a href="">About</a>
-        <a href="">Contact</a>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/products"}>Products</Link>
+        <Link to={"/account"}>Account</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
       </div>
       <div className="header-R">
         <div className='cartBox'>
@@ -20,8 +22,8 @@ export default function Header() {
           </span>
         </div>
         <div className='accountBox'>
-          <button>Sign In</button>
-          <button>Sign Up</button>
+          <button onClick={()=>{navigate("/login")}}>Sign In</button>
+          <button onClick={()=>{navigate("/register")}}>Sign Up</button>
         </div>
       </div>
     </div>
