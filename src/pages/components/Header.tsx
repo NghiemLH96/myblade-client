abbc96efe8afc97e565cb60583f2c488af64ed9d
Nghiem Le <nghiemle@Nghiems-MacBook-Pro.local>
2024-01-12 21:50:06 +0700

    client

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,8 +1,7 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/blade-high-resolution-logo-black-removebg-preview.png'
 
 export default function Header() {
-  const navigate = useNavigate()
   return (
     <div className='pageHeader'>
       <section className='header_T'>
