f05d0fc1acff7d25ee0e50b7b9d084f6acef9161
Nghiem Le <nghiemle@Nghiems-MacBook-Pro.local>
2024-01-12 21:11:22 +0700

    Client

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -5,27 +5,49 @@ export default function Header() {
   const navigate = useNavigate()
   return (
     <div className='pageHeader'>
-      <div className="header-L">
-        <img src={logo} className="logo" alt="" />
-      </div>
-      <div className="header-M">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/products"}>Products</Link>
-        <Link to={"/account"}>Account</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contact"}>Contact</Link>
-      </div>
-      <div className="header-R">
-        <div className='cartBox'>
-          <span className="material-symbols-outlined cartIcon">
-            shopping_cart
-          </span>
+      <section className='header_T'>
+        <div className="header-L">
+          <img src={logo} className="logo" alt="" />
         </div>
-        <div className='accountBox'>
-          <button onClick={()=>{navigate("/login")}}>Sign In</button>
-          <button onClick={()=>{navigate("/register")}}>Sign Up</button>
+        <div className="header-M">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/products"}>Products</Link>
+          <Link to={"/account"}>Account</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/contact"}>Contact</Link>
         </div>
-      </div>
+        <div className="header-R">
+          <div className='accountBox'>
+            <div className='menuBox'>
+              <span className="material-symbols-outlined menuIcon">
+                menu
+              </span>
+              <img className='userAvatar' src="https://vieclam.vimaru.edu.vn/assets/uploads/no_avatar.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='header-B'>
+        <div className='searchField'>
+          <div className='inputField location'>
+            <label htmlFor="">Location</label>
+            <input type="text" />
+          </div>
+          <div className='inputField checkIn'>
+            <label htmlFor="">Check In</label>
+            <input type="text" />
+          </div>
+          <div className='inputField checkOut'>
+            <label htmlFor="">Check Out</label>
+            <input type="text" />
+          </div>
+          <div className='inputField Paxes'>
+            <label htmlFor="">Paxes</label>
+            <input type="text" />
+          </div>
+          <button>Search</button>
+        </div>
+      </section>
     </div>
   )
 }
